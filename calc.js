23d46be3487b29e41effd17910fd58327886cd5f
Damián Di Febo <difebodamian@gmail.com>
2022-11-03 22:01:47 -0300

    diseño terminado

---
+++ calc.js
@@ -0,0 +1,11 @@
+const result = document.querySelector(".result");
+const numbers = document.querySelector(".numbers");
+const btn = document.querySelectorAll(".btn")
+btn.forEach((i) => {
+    console.log(i.outerText)
+})
+
+
+function cargarEventListeners() {
+
+}
